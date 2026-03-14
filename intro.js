04e6255267c31d2747a03634d9d590b2dfc3a634
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-03-14 14:45:08 +0100

    js pro

--- intro.js
+++ intro.js
@@ -9,6 +9,23 @@ let programmer = {
 		}
 	};
 
+function createProgrammer (name, preferredLanguage) {
+	return {
+		name,
+		preferredLanguage,
+		writeCode() {
+			console.log(`${this.name} write ${preferredLanguage} code.`);
+		},
+		
+		drinkCoffee() {
+			consolo.log(`${this.name} drinks coffee.`);
+		}
+	}
+}
+
+const newProgrammer = createProgrammer("Alice", "JavaScript");
+
+newProgrammer.writeCode();
 
 let groceryListItem = {
 		name: "Apples",
@@ -18,4 +35,41 @@ let groceryListItem = {
 		}
 	};
 
-groceryListItem.display();
+function createGrocery (name, quantity) {
+		return{
+			name,
+			quantity,
+			display: function() {
+				console.log(`${this.quantity} x ${this.name}`);	
+			}
+
+		}
+	}
+
+const newGrocery = createGrocery("Apples", 4);
+
+function Programmer(name, preferredLanguage) {
+	this.name = name;
+	this.preferredLanguage = preferredLanguage;
+	this.writeCode = function() {
+			console.log(`${this.name} writes ${this.preferredLanguage} code.`);
+		}
+	this.drinksCoffee = function() {
+		console.log(`${this.name} drinks coffee.`);
+	};
+
+}
+
+const newProgram = new Programmer("Alice", "Javascript");
+
+function GroceryItem (name, quantity) {
+	this.name = name;
+	this.quantity = quantity;
+	this.display = function() {
+		console.log(`${this.quantity} x ${this.name}`);
+	};
+}
+
+const newGroceryItem = new GroceryItem("John", 4);
+
+newGroceryItem.display();
