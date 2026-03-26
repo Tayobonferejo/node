ab0dc75b72f9450536ad495c45fd36cb4d18eef5
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-03-26 13:16:53 +0100

    descriptor

--- descriptor.js
+++ descriptor.js
@@ -2,8 +2,23 @@ let person = {
     name: "Steven"
 };
 
-console.log(person);
+// console.log(person);
 
-for (let value in person) {
-    console.log(person[value]);
+// for (let value in person) {
+//     console.log(person[value]);
+// }
+
+Object.defineProperty(person, "name", {
+    writable: false,
+    enumerable: false,
+    configurable: true
+});
+
+for (let key in person) {
+    console.log(key);
 }
+
+// console.log(Object.keys(person));
+// let objectBase = Object.getPrototypeOf(person);
+// const propertyDescritptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
+// console.log(propertyDescritptor);
