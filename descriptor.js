49f00292f03dc02651547ebd9e06fa8a551ac7f8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-03-30 23:29:33 +0100

    descriptor

--- descriptor.js
+++ descriptor.js
@@ -22,3 +22,36 @@ for (let key in person) {
 // let objectBase = Object.getPrototypeOf(person);
 // const propertyDescritptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
 // console.log(propertyDescritptor);
+
+
+function Circle () {
+}
+
+let obj = {};
+
+console.log(obj.__proto__);
+console.log(Object.prototype);
+
+
+function Programmer(name, preferredLanguage) {
+    this.name  = name;
+    this.preferredLanguage = preferredLanguage;
+}
+
+Programmer.prototype.writeCode = function() {
+    console.log(`${this.name} write code in ${this.preferredLanguage}`);
+};
+
+Programmer.prototype.toString = function () {
+    return `Programmer: ${this.name}, Language: ${this.preferredLanguage}`;
+};
+
+const jsProgrammer = new Programmer("John", "JavaScript");
+jsProgrammer.writeCode();
+console.log(jsProgrammer);
+
+for (let key in Programmer) {
+    console.log(key);
+}
+
+console.log(jsProgrammer.hasOwnProperty("name"));
