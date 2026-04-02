46349109ae8da72caee3de847acf0980d91b57fd
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-04-02 11:30:28 +0100

    constuctor

--- constructor.js
+++ constructor.js
@@ -1,15 +1,35 @@
-function Programmer (name, specialiation) {
+function Programmer (name, specialisation) {
     this.name = name;
-    this.specialisation = specialiation
+    this.specialisation = specialisation
 }
 
 function FrontEndProgrammer(name, specialisation, perferredFramework) {
-    Programmer.call(this, name, specialiation);
+    Programmer.call(this, name, specialisation);
     this.perferredFramework = perferredFramework;
 }
 
+function BackendProgrammer(name, specialisation) {
+    Programmer.call(this, name, specialisation);
+}
+
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
+extend(FrontEndProgrammer, Programmer);
+extend(BackendProgrammer,Programmer);
+
+Programmer.prototype.code = function() {
+    console.log(`${this.name} starts coding.`);
+}
 const steven = new FrontEndProgrammer("Steven", "Front-end", "React");
+
+const johnny = new BackendProgrammer("Johnny", "Backend");
 
 console.log(steven.name);
 console.log(steven.specialisation);
 console.log(steven.perferredFramework);
+
+steven.code();
+johnny.code();
